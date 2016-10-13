c03ad976532e56567d34aa4a912cccbdde2e3027
unknown <augusto.naraujo@CAS554828W07I14.senacsp.edu.br>
2016-10-13 18:50:12 -0300

    A terminar

---
+++ gamescrudpii02/js/lightbox.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+	$('.lightbox').click(function(){
+		$('.background, .box').animate({'opacity':'.60'}, 500, 'linear');
+		$('.box').animate({'opacity':'1.00'}, 500, 'linear');
+		$('.background, .box').css('display', 'block');					
+	});
+	
+	$('.close').click(function(){
+		$('.background, .box').animate({'opacity':'0'}, 500, 'linear', function(){
+			$('.background, .box').css('display', 'none');
+		});
+	});
+	
+	$('.background').click(function(){
+		$('.background, .box').animate({'opacity':'0'}, 500, 'linear', function(){
+			$('.background, .box').css('display', 'none');
+		});
+	});
+});		
