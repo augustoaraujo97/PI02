b297f37caa5d700b14a5a028e2eb464167c47cb1
augustoaraujo97 <augustoaraujo.97@gmail.com>
2016-10-24 21:55:29 -0200

    Fazer a paginação

---
+++ games24-10/area/index.php
@@ -0,0 +1,136 @@
+<?php
+
+require('../db/db.php');
+
+$msg = '';
+$result = '';
+
+// --------------------------------------------------------------------Pesquisa --------------------------------------------------------------------
+if(isset($_POST['btnPesquisar'])) {
+	if (!empty($_POST['txtPesquisa'])) {
+			$pesquisa = $_POST['txtPesquisa'];
+			// Criar um função para evitar injection
+
+			$query = odbc_exec($db, "SELECT 
+										codArea, 
+										descricao 
+									FROM 
+										area 
+									WHERE descricao LIKE '%$pesquisa%'" ); 
+			$butt = "<button id='btnVoltar' name='btnVoltar'><a href='index.php'>Voltar</a></button>";
+	} else {
+		$query = odbc_exec($db, 'SELECT 
+									codArea,
+									descricao
+								FROM 
+									Area');
+	}
+} else {
+	// Valor default
+	$query = odbc_exec($db, 'SELECT 
+								codArea,
+								descricao
+							FROM 
+								Area');
+}
+
+
+
+//-------------------------------------------------------------------- Exibe na grade--------------------------------------------------------------------
+while($result = odbc_fetch_array($query)){
+	$areas[$result['codArea']] = utf8_encode($result['descricao']);
+}
+
+//--------------------------------------------------------------------DELETE--------------------------------------------------------------------
+if(isset($_GET['dcod'])){
+	if(is_numeric($_GET['dcod'])){
+		//verifica se existe dependencia
+		$descricao = odbc_exec($db,' SELECT 
+										descricao 
+									FROM 
+										Assunto 
+									WHERE codArea='.$_GET['dcod']);
+		$num = odbc_num_rows($descricao);
+		if($num > 0){
+			$msg = "Não foi possivel deletar <br> A area possui dependencia com o(s) campo(s): ";
+			while($pega = odbc_fetch_array($descricao)){
+				$msg .= "".$pega['descricao'].", ";
+			}
+		}else{
+			if(!odbc_exec($db, "DELETE FROM 
+									Area 
+								WHERE 
+									codArea =".$_GET['dcod'])){
+				$msg = "Não foi possivel apagar o dado";
+			}else{
+				header("Location: index.php");
+			}
+		}
+	}else{
+		$msg = "ERRO : ID não valido";
+	}
+}
+
+//-------------------------------------------------------------------- COMEÇO DOS CRUDS--------------------------------------------------------------------
+// --------------------------------------------------------------------INSERT--------------------------------------------------------------------
+if(isset($_POST['btnInclude'])) {
+	$area = $_POST['txtInclude'];
+	$area = preg_replace("/[^a-zA-Z0-9 -]/",'',$_POST['txtInclude']);
+	
+	$prepare = odbc_prepare($db, "INSERT INTO 
+									Area (descricao) 
+								  VALUES 
+									(?)");
+	
+	if(!odbc_execute($prepare, array($area))){
+							
+		$msg = "Não foi possivel inserir";
+	}else {
+		header("Location: index.php");
+	}
+}
+
+//--------------------------------------------------------------------EDITAR--------------------------------------------------------------------
+if(isset($_GET['ecod']) && is_numeric($_GET['ecod'])){
+
+	$select = odbc_exec($db, "SELECT 
+								codArea, 
+								descricao 
+							FROM 
+								Area 
+							WHERE codArea = ".$_GET['ecod']);
+	$result = odbc_fetch_array($select);
+	include_once('templats/crudArea.php');
+} else {
+	$result = '';
+}
+
+//--------------------------------------------------------------------UPDATE--------------------------------------------------------------------
+if(isset($_POST['btnAlterar']  )){
+	if(is_numeric($_GET['ecod'])){
+		$area = $_POST['txtDescricao'];
+		$area = preg_replace("/[^a-zA-Z0-9 -]/",'',$_POST['txtDescricao']);
+		
+		$prepare = odbc_prepare($db, "UPDATE 
+										Area 
+									  SET 
+										descricao = ? 
+									  WHERE 
+									   codArea = {$_GET['ecod']}");
+		
+		if(odbc_execute($prepare, array($area))){
+			header("Location: index.php");
+		}
+	}else{
+		$msg = "Não foi possível atualizar";
+	}
+}
+
+//-------------------------------------------------------------------- FIM UPDATE--------------------------------------------------------------------
+$msg = utf8_encode($msg);
+if(isset($_POST['btnNovo']) || isset($_GET['ecod']) ){
+	include_once('templats/crudArea.php');	
+}else{
+	include_once('templats/area.php');	
+}
+?>
---
+++ games24-10/assunto/index.php
@@ -0,0 +1,149 @@
+<?php
+
+require('../db/db.php');
+
+$msg = '';
+$result = ' ';
+
+// --------------------------------------------------------------------------Pesquisa --------------------------------------------------------------------------
+if(isset($_POST['btnPesquisar'])) {
+	if (!empty($_POST['txtPesquisa'])) {
+			$pesquisa = $_POST['txtPesquisa'];
+			// Criar um função para evitar injection
+
+			$query = odbc_exec($db, "SELECT 
+										ass.codAssunto, 
+										ass.descricao as assdescricao,
+										ass.codArea ,
+										ar.descricao as ardescricao
+									FROM 
+										assunto as ass
+									LEFT JOIN
+										area as ar ON ar.codArea = ass.codArea
+									WHERE descricao LIKE '%$pesquisa%'" ); 
+			$butt = "<button id='btnVoltar' name='btnVoltar'><a href='index.php'>Voltar</a></button>";
+	} else {
+			$query = odbc_exec($db, "SELECT 
+										ass.codAssunto, 
+										ass.descricao as assdescricao,
+										ass.codArea ,
+										ar.descricao as ardescricao
+									FROM 
+										assunto as ass
+									LEFT JOIN
+										area as ar ON ar.codArea = ass.codArea");
+	}
+} else {
+	// Valor default
+				$query = odbc_exec($db, "SELECT 
+										ass.codAssunto, 
+										ass.descricao as assdescricao,
+										ass.codArea ,
+										ar.descricao as ardescricao
+									FROM 
+										assunto as ass
+									LEFT JOIN
+										area as ar ON ar.codArea = ass.codArea");
+}
+
+// ---------------------------------------------------------- GRADES ---------------------------------------------------------------------
+//AREA
+$queryArea = odbc_exec($db, "SELECT
+								codArea,
+								descricao
+							FROM
+								Area");
+while($resultArea = odbc_fetch_array($queryArea)){
+	$areas[$resultArea['codArea']] = utf8_encode($resultArea['descricao']);
+}
+
+while($result = odbc_fetch_array($query)){
+	$assuntos[$result['codAssunto']]['assdescricao'] = utf8_encode($result['assdescricao']);
+	$assuntos[$result['codAssunto']]['codArea'] = $result['codArea'];
+	$assuntos[$result['codAssunto']]['ardescricao'] = utf8_encode($result['ardescricao']);
+}
+// --------------------------------------------------------------------------DELETE--------------------------------------------------------------------------
+if(isset($_GET['dcod'])){
+	if(is_numeric($_GET['dcod'])){
+		//verifica se existe dependencia
+		
+		if(!odbc_exec($db, "DELETE FROM 
+								Assunto
+							WHERE 
+								codAssunto =".$_GET['dcod'])){
+			$msg = "Não foi possível deletar.";
+		}else{
+			header("Location: index.php");
+		}
+		
+	}else{
+		$msg = "ERRO : ID não valido";
+	}
+}
+
+// -------------------------------------------------------------------------------INSERT ------------------------------------------------------------------------
+if(isset($_POST['btnInclude'])) {
+	$assunto = $_POST['txtInclude'];
+	$assunto = preg_replace("/[^a-zA-Z0-9 -]/",'',$_POST['txtInclude']);
+	$codArea = intval($_POST['codArea']);
+	
+	$prepare = odbc_prepare($db, "INSERT INTO 
+									Assunto (descricao, codArea) 
+								VALUES 
+									(?, $codArea)");
+	if(!odbc_execute($prepare, array($assunto))){
+		$msg = "Não foi possivel inserir";
+
+	}else {
+		header("Location: index.php");
+	}
+}
+
+//-------------------------------------------------------------------------------EDITAR--------------------------------------------------------------------------
+if(isset($_GET['ecod']) && is_numeric($_GET['ecod'])){
+	$select = odbc_exec($db, "SELECT 
+								ass.codAssunto, 
+								ass.descricao as assdescricao,
+								ass.codArea ,
+								ar.descricao as ardescricao
+							FROM 
+								assunto as ass
+							LEFT JOIN
+								area as ar ON ar.codArea = ass.codArea
+							WHERE 
+								ass.codAssunto = ".$_GET['ecod']);
+	$result = odbc_fetch_array($select);
+} else {
+	$result = '';
+}
+
+// --------------------------------------------------------------------------UPDATE-------------------------------------------------------------------
+if(isset($_POST['btnAssuntoUpdate']  )){
+	if(is_numeric($_GET['ecod'])){
+		$assunto = $_POST['txtAssuntoUpdate'];
+		$assunto = preg_replace("/[^a-zA-Z0-9 -]/",'',$_POST['txtAssuntoUpdate']);
+		$codArea = intval($_POST['codArea']);
+		
+		$prepare = odbc_prepare($db, "UPDATE
+										Assunto
+									SET
+										descricao = ?
+										codArea = $codArea
+									WHERE
+										codAssunto = {$_GET['ecod']}");
+		if(odbc_execute($prepare, array($assunto))){
+			header("Location: index.php");
+		}
+	}else{
+		$msg = "Não foi possível atualizar";
+	}
+}
+
+// -------------------------------------------------------------------------- FIM UPDATE-------------------------------------------------------------------
+if(isset($_POST['btnNovo']) || isset($_GET['ecod']) ){
+	include_once("templats/crudAssunto.php");
+}else{
+	include("templats/assunto.php");	
+}
+
+?>
